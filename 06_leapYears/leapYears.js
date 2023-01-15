50448fcd0411985b81aa1cf0ad5184a311ccd7e8
bagwuel <mbagwu39@gmail.com>
2023-01-15 18:42:56 +0100

    solve first javascript ex 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (isNaN(year)) 
+        return ('ERROR');
+    let myYear = Number(year);
+    if (myYear % 4 !== 0) {
+        return (false)
+    }
+    else {
+        if ((myYear % 100 === 0) && (myYear % 400 !== 0))
+            return (false);
+        return (true)
+    }
 };
 
 // Do not edit below this line
