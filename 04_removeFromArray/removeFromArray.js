3fd8f50b9e07459bb8dee10cb3a1f87df05adadf
bagwuel <mbagwu39@gmail.com>
2023-01-15 17:09:28 +0100

    solve the first JavaScript Ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    if (!Array.isArray(arguments[0]))
+        return;
+    let length = arguments.length
+    for (let i = 1; i < length; i++) {
+        while ((arguments[0].indexOf(arguments[i])) !== -1) {
+            arguments[0].splice(arguments[0].indexOf(arguments[i]), 1)
+        }
+    }
+    return (arguments[0])
 };
 
 // Do not edit below this line
