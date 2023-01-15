98fbecb7a3cc7aeff1d050f41a3f8cf0da5a51da
bagwuel <mbagwu39@gmail.com>
2023-01-15 22:20:22 +0100

    solve first javascript ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    return str === str.split('').reverse().join('');
 };
 
-// Do not edit below this line
+// // Do not edit below this line
 module.exports = palindromes;
