71e7cf2ef97b9207d6e952141e4c71f43a4304f5
bagwuel <mbagwu39@gmail.com>
2023-01-15 16:56:35 +0100

    solve the first JavaScript Ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = '';
+    let length = string.length;
+    for (let i = length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return (reversedString);
 };
 
 // Do not edit below this line
