a037ea4c7e2b818ed6f12be4794e9d5833206649
bagwuel <mbagwu39@gmail.com>
2023-01-15 18:17:24 +0100

    solve first javascript ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    for (let i = 0; i < arguments.length; i++) {
+        if ((arguments[i] < 0) || typeof(arguments[i]) !== 'number') {
+            return ('ERROR')
+        }
+    }
+    let sum = 0;
+    let min = Math.min(...arguments);
+    let max = Math.max(...arguments);
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return (sum);
 };
 
 // Do not edit below this line
