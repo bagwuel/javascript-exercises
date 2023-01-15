cd250fd2b3458a35996066dc838f8a28e952b334
bagwuel <mbagwu39@gmail.com>
2023-01-15 19:11:14 +0100

    solve first javascript ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let convertedTemperature = (temperature - 32) * (5 / 9);
+  let to_One_Decimal = Math.round(convertedTemperature * 10) / 10;
+  return (to_One_Decimal);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let convertedTemperature = (temperature * (9 / 5)) + 32;
+  let to_One_Decimal = Math.round(convertedTemperature * 10) / 10;
+  return (to_One_Decimal);
 };
 
 // Do not edit below this line
