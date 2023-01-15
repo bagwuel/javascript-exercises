41b3e93501e45b56bfef4aea747bbbb868488cc0
bagwuel <mbagwu39@gmail.com>
2023-01-15 16:45:17 +0100

    solve the first JavaScript Ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringRepeat = '';
+    if (num < 0)
+        return ('ERROR')
+    for (let i = 0; i < num; i++) {
+        stringRepeat += string;
+    }
+    return (stringRepeat);
 };
 
 // Do not edit below this line
