6f68e2a167c97bdcd7156a930a253a6ddbaddf67
bagwuel <mbagwu39@gmail.com>
2023-01-15 22:46:12 +0100

    solve first javascript ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof (Number(num)) !== "number")
+        return ('OOPS')
+    else
+        num = Number(num)
+    if (num < 1)
+        return ('OOPS')
+    if (num === 1 || num === 2) {
+        return (1);
+    }
+    let nth_fibo = 0;
+    let n1 = 1;
+    let n2 = 1;
+    for (let i = 0; i < num - 2; i++) {
+        nth_fibo = n1 + n2;
+        n1 = n2;
+        n2 = nth_fibo
+    }
+    return (nth_fibo);
 };
 
 // Do not edit below this line
