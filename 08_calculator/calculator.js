3c028f052f785ebfd67310b3a69bf5f1d2d34e94
bagwuel <mbagwu39@gmail.com>
2023-01-15 20:00:08 +0100

    solve first javascript ex 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,62 @@
 const add = function() {
-	
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+	return (sum);
 };
 
 const subtract = function() {
-	
+	let difference = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    difference -= arguments[i]
+  }
+  return (difference);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== "number") {
+      return ('ERROR')
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return (sum);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] !== "number") {
+      return ('ERROR')
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    product *= array[i];
+  }
+  
+  return (product);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  if ((typeof num1 !== "number") || (typeof num2 !== "number"))
+    return ('ERROR');
+	return (Math.pow(num1, num2))
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let num_Factorial = 1;
+  if (typeof num !== "number")
+    return ('ERROR');
+	if (num === 0 || num === 1)
+    return (1);
+  for (let i = num; i > 0; i--) {
+    num_Factorial *= i;
+  }
+  return (num_Factorial);
 };
 
 // Do not edit below this line
