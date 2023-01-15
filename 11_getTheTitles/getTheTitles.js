b639a4c26ad0c1a05b856772d9e9f388de147c86
bagwuel <mbagwu39@gmail.com>
2023-01-15 23:05:16 +0100

    solve first javascript ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titleArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].hasOwnProperty('title')) {
+            titleArray.push(array[i]['title'])
+        }
+        else
+            continue;
+    }
+    return (titleArray);
 };
 
 // Do not edit below this line
