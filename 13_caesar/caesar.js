efccf0c8d869e81652975a41605a55f57f3bff6c
bagwuel <mbagwu39@gmail.com>
2023-01-16 11:25:51 +0100

    solve first javascript ex 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,20 @@
-const caesar = function() {
-
-};
-
+const caesar = function(string, number) {
+    const shift = number % 26;
+    let newString = '';
+    
+    for (let i = 0; i < string.length; i++) {
+      let code = string.charCodeAt(i);
+      if (code >= 97 && code <= 122) {
+          newString += String.fromCharCode((code - 97 + shift + 26) % 26 + 97);
+          } else if (code >= 65 && code <= 90) {
+            newString += String.fromCharCode((code - 65 + shift + 26) % 26 + 65);
+          } else {
+            newString += string[i];
+          }
+        }
+      
+        return newString;
+      
+  };
 // Do not edit below this line
 module.exports = caesar;
